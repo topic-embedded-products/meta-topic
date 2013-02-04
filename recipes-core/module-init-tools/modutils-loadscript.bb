a3f4d583cf61fdcbd941ca1155cef7040a45d931
Mike Looijmans <mike.looijmans@topic.nl>
2013-02-04 10:36:06 +0100

    modutils-loadscript: Also load HID and INPUT devices, fix license checksum
    To load e.g. touchscreens, mice and keyboard drivers dynamically.
    License checksum "lines" was incorrect, fixed it and added the correct
    checksum.

--- recipes-core/module-init-tools/modutils-loadscript.bb
+++ recipes-core/module-init-tools/modutils-loadscript.bb
@@ -1,8 +1,9 @@
 SECTION = "base"
 DESCRIPTION = "modutils configuration files"
 LICENSE = "GPLv2"
-LIC_FILES_CHKSUM = "file://modload.sh;lines=2;md5=9a17195430e0508eaf9830505cd89be4"
+LIC_FILES_CHKSUM = "file://modload.sh;beginline=2;endline=2;md5=3b6e5b2caf81c241a5956ed7691327ab"
 SRC_URI = "file://modload.sh"
+PV = "2"
 PR = "r0"
 
 INITSCRIPT_NAME = "modload.sh"
--- recipes-core/module-init-tools/modutils-loadscript/modload.sh
+++ recipes-core/module-init-tools/modutils-loadscript/modload.sh
@@ -2,8 +2,8 @@
 # Licensed under GPLv2
 # Load kernel modules for hotpluggable devices
 
-# Probe all USB modules so that drivers load automatically
-grep -h '^usb:' `find /sys/devices/ -name modalias` | while read m
+# Probe all USB/HID/input modules so that drivers load automatically
+grep -h '^usb:\|^input:\|^hid:' `find /sys/devices/ -name modalias` | while read m
 do
 	modprobe $m > /dev/null 2> /dev/null
 done
