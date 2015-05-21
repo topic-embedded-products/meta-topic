3a455666d9c568661ea2913509bb17a338ccb065
Mike Looijmans <mike.looijmans@topic.nl>
2015-05-21 08:09:43 +0200

    Fix install scripts for current Ubuntu versions and machine names
    The "install_to_sd_my-miami-70*.sh" scripts had the wrong MACHINE
    environment, set the machine to one that matches the script's name
    and that actually exists.
    
    Ubuntu versions after 12 mount media at /media/$USER, while 12
    mounts them at /media. Adapt the script to detect which is being
    used and make it work on both versions.
    Introduce extra variables IMAGE_ROOT and FPGA_BOOT_IMAGE.
    Install ubi and squashfs images into the 'data' partition if possible.

--- scripts/install_to_sd_common.sh
+++ scripts/install_to_sd_common.sh
@@ -12,27 +12,34 @@ then
 	echo "along."
 	exit 1
 fi
-if [ ! -w /media/boot ]
-then
-	echo "/media/boot is not accesible. Are you root (sudo me),"
-	echo "is the SD card inserted, and did you partition and"
-	echo "format it with partition_sd_card.sh?"
-	exit 1
-fi
-if [ ! -w /media/rootfs ]
-then
-	echo "/media/rootfs is not accesible. Are you root (sudo me),"
-	echo "is the SD card inserted, and did you partition and"
-	echo "format it with partition_sd_card.sh?"
-	exit 1
-fi
 if [ -z "${IMAGE_ROOT}" ]
 then
 	IMAGE_ROOT=tmp-glibc/deploy/images/${MACHINE}
 fi
+# Ubuntu <14 uses /media for mounts, Ubuntu 14 uses /media/$USER
+if [ -d /media/${SUDO_USER} ]
+then
+	MEDIA=/media/${SUDO_USER}
+else
+	MEDIA=/media
+fi
+if [ ! -w ${MEDIA}/boot ]
+then
+	echo "${MEDIA}/boot is not accesible. Are you root (sudo me),"
+	echo "is the SD card inserted, and did you partition and"
+	echo "format it with partition_sd_card.sh?"
+	exit 1
+fi
+if [ ! -w ${MEDIA}/rootfs ]
+then
+	echo "${MEDIA}/rootfs is not accesible. Are you root (sudo me),"
+	echo "is the SD card inserted, and did you partition and"
+	echo "format it with partition_sd_card.sh?"
+	exit 1
+fi
 if [ ! -f ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.tar.gz ]
 then
-	echo "Image '${IMAGE}' does not exist, cannot flash it:"
+	echo "Image '${IMAGE}' does not exist, cannot flash it."
 	echo ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.tar.gz
 	exit 1
 fi
@@ -47,41 +54,64 @@ do
 done
 if [ -z "${DTB}" ]
 then
-	DTB="uImage-${MACHINE}-dyplo.dtb"
+	DTB="uImage-*-adv7511.dtb"
 fi
 set -e
+if [ -z "${SD_BOOTSCRIPT}" ]
+then
+	SD_BOOTSCRIPT=autorun.scr
+fi
+if [ -d ${MEDIA}/data ]
+then
+	MEDIA_DATA=${MEDIA}/data
+else
+	MEDIA_DATA=${MEDIA}/boot
+fi
 echo "Writing boot..."
-rm -f /media/boot/*.ubi
-cp ${IMAGE_ROOT}/BOOT.bin /media/boot/BOOT.BIN
-if [ -e ${IMAGE_ROOT}/u-boot.img ]
+rm -f ${MEDIA}/boot/*.ubi ${MEDIA}/boot/*.squashfs-lzo
+if [ -e ${IMAGE_ROOT}/BOOT.bin ]
 then
-	cp ${IMAGE_ROOT}/u-boot.img /media/boot/
+	cp ${IMAGE_ROOT}/BOOT.bin ${MEDIA}/boot/BOOT.BIN
+	if [ -e ${IMAGE_ROOT}/u-boot.img ]
+	then
+		cp ${IMAGE_ROOT}/u-boot.img ${MEDIA}/boot/
+	fi
+	cp ${IMAGE_ROOT}/${SD_BOOTSCRIPT} ${MEDIA}/boot/autorun.scr
+	cp ${IMAGE_ROOT}/uImage ${MEDIA}/boot/
+	cp ${IMAGE_ROOT}/${DTB} ${MEDIA}/boot/devicetree.dtb
+else
+	tar xaf ${IMAGE_ROOT}/boot.tar.gz --no-same-owner -C ${MEDIA}/boot
 fi
-cp ${IMAGE_ROOT}/*.scr /media/boot/
-cp ${IMAGE_ROOT}/uImage /media/boot/
-cp ${IMAGE_ROOT}/${DTB} /media/boot/devicetree.dtb
-if [ -f ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.ubi ]
-then
-	cp ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.ubi /media/boot/
-fi
+for FS in ubi squashfs-lzo squashfs-xz
+do
+	if [ -f ${IMAGE_ROOT}/${IMAGE}*-${MACHINE}.${FS} ]
+	then
+		cp ${IMAGE_ROOT}/${IMAGE}*-${MACHINE}.${FS} ${MEDIA_DATA}
+	fi
+done
 echo "Writing rootfs..."
-if [ ! -f dropbear_rsa_host_key -a -f /media/rootfs/etc/dropbear/dropbear_rsa_host_key ]
+if [ ! -f dropbear_rsa_host_key -a -f ${MEDIA}/rootfs/etc/dropbear/dropbear_rsa_host_key ]
 then
-	cp /media/rootfs/etc/dropbear/dropbear_rsa_host_key .
+	cp ${MEDIA}/rootfs/etc/dropbear/dropbear_rsa_host_key .
 	chmod 666 dropbear_rsa_host_key
 fi
-rm -rf /media/rootfs/*
-tar xzf ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.tar.gz -C /media/rootfs
+rm -rf ${MEDIA}/rootfs/*
+tar xzf ${IMAGE_ROOT}/${IMAGE}-${MACHINE}.tar.gz -C ${MEDIA}/rootfs
 if [ -f dropbear_rsa_host_key ]
 then
-	install -d /media/rootfs/etc/dropbear
-	install -m 600 dropbear_rsa_host_key /media/rootfs/etc/dropbear/dropbear_rsa_host_key
+	install -d ${MEDIA}/rootfs/etc/dropbear
+	install -m 600 dropbear_rsa_host_key ${MEDIA}/rootfs/etc/dropbear/dropbear_rsa_host_key
 fi
+if [ ! -z "${FPGA_BOOT_IMAGE}" ]
+then
+	cp ${MEDIA}/rootfs/${FPGA_BOOT_IMAGE} ${MEDIA}/boot/fpga.bin
+fi
+
 if [ $DO_UNMOUNT -ne 0 ]
 then
 	sleep 1
 	echo -n "Unmounting"
-	for p in /media/boot /media/rootfs /media/data
+	for p in ${MEDIA}/boot ${MEDIA}/rootfs ${MEDIA}/data
 	do
 		if [ -d $p ]
 		then
--- scripts/install_to_sd_my-miami-7015.sh
+++ scripts/install_to_sd_my-miami-7015.sh
@@ -1,4 +1,4 @@
 IMAGE=my-image
-MACHINE=topic-miami-7015
+MACHINE=topic-miami-florida-med-xc7z015
 DTB=uImage-topic-miami-florida-med.dtb
 source `dirname $0`/install_to_sd_common.sh
