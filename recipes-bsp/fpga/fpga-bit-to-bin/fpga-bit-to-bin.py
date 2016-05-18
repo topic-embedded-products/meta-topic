a0673f8cecb80b8ddb900fa6a7561cfdcf352827
Niek van Agt <niek.van.agt@topic.nl>
2016-05-18 17:04:09 +0200

    Change bit-to-bin conversion script to add a header to partial bitstreams
    This header is used by the Dyplo driver to determine if a parital
    bitstream that is about to be programmed is compatible with the
    programmed full bitstream.

--- recipes-bsp/fpga/fpga-bit-to-bin/fpga-bit-to-bin.py
+++ recipes-bsp/fpga/fpga-bit-to-bin/fpga-bit-to-bin.py
@@ -37,19 +37,64 @@ l = short.unpack(bitfile.read(2))[0]
 d = bitfile.read(l)
 print "Design name:", d
 
+# If bitstream is a partial bitstream, get some information from filename and header 
+if d.find("PARTIAL=TRUE") != -1:
+	print "Partial bitstream"
+	partial = True;
+	
+	# Get node_nr from filename (last (group of) digits)
+	for i in range (len(args.bitfile) - 1, 0, -1):
+		if args.bitfile[i].isdigit():
+			pos_end = i + 1
+			for j in range (i - 1, 0, -1):
+				if not args.bitfile[j].isdigit():
+					pos_start = j + 1
+					break
+			break
+	if pos_end != 0 and pos_end != 0:
+		node_nr = int(args.bitfile[pos_start:pos_end])
+	else:
+		node_nr = 0
+	print "NodeID:", node_nr
+	
+	# Get 16 least significant bits of UserID in design name
+	pos_start = d.find("UserID=") 
+	if pos_start != -1:
+		pos_end = d.find(";", pos_start)
+		pos_start = pos_end - 4
+		userid = d[pos_start:pos_end]
+		print "UserID:", userid	
+	
+else:
+	print "Full bitstream"
+	partial = False
+	node_nr = 0
+
 KEYNAMES = {'b': "Partname", 'c': "Date", 'd': "Time"}
 
 while 1:
 	k = bitfile.read(1)
 	if not k:
+		bitfile.close()
 		raise Exception, "unexpected EOF"
 	elif k == 'e':
 		l = ulong.unpack(bitfile.read(4))[0]
-		print "found binary data:", l
+		print "Found binary data:", l
 		d = bitfile.read(l)
 		if args.flip:
+			print "Flipping data..."
 			d = flip32(d)
-		open(args.binfile, 'wb').write(d)
+		# Open bin file
+		binfile = open(args.binfile, 'wb')
+		# Write header if it is a partial
+		if partial:
+			binfile.write(struct.pack("B", 0))
+			binfile.write(struct.pack("B", node_nr))
+			binfile.write(struct.pack(">H", int("0x" + userid, 16)))
+		# Write the converted bit-2-bin data
+		print "Writing data..."
+		binfile.write(d)
+		binfile.close()
 		break
 	elif k in KEYNAMES:
 		l = short.unpack(bitfile.read(2))[0]
@@ -59,4 +104,5 @@ while 1:
 		print "Unexpected key: ", k
 		l = short.unpack(bitfile.read(2))[0]
 		d = bitfile.read(l)
-
+		
+bitfile.close()
