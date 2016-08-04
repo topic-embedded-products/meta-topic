topic-embedded-products/meta-topic

commit f8c7aaecc1d947e7600b7fea6c471273ce281207

Add topic-miamiplus SOM family and topic-miamiplus-florida-test-xc7z045 machine
First topic-miamiplus board will contain a xc7z045 device.
There will also be a (production) test board for these boards.
Hence the first 7045 board will be called "topic-miamiplus-florida-test-xc7z045".

This adds the kernel configuration and devicetree.

recipes-kernel/linux-zynq/linux-topic.bb
@@ -4,7 +4,7 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
 
 KBRANCH = "topic-miami"
-SRCREV = "0a03e1eccda1ee263dad5c02a844f9f7eb2a2e03"
+SRCREV = "bbf9a879a848412520ab263c277acc10a5747845"
 LINUX_VERSION = "4.4"
 
 inherit kernel
@@ -38,7 +38,7 @@ KERNEL_DEVICETREE_topic-miami = "\
 	topic-miami-florida-mio-nand-dyplo.dtb \
 	topic-miami-florida-pci.dtb \
 	topic-miami-florida-pci-pt.dtb \
-	topic-miami-vice.dtb \
+	topic-miamiplus-florida-test.dtb \
 	"
 # See http://permalink.gmane.org/gmane.linux.kernel.commits.head/371588
 KERNEL_EXTRA_ARGS += "LOADADDR=0x00008000"

conf/machine/include/topic-miamiplus.inc
@@ -1,0 +1,8 @@
+# The miami plus boards have extra I/O connectors and slightly different hardware
+require conf/machine/include/topic-miami.inc
+
+# This makes the "topic-miamiplus" override work
+SOM_FAMILY = "topic-miamiplus"
+
+# But the "topic-miami" override applies as well, with lower priority
+MACHINEOVERRIDES =. "topic-miami:"

conf/machine/topic-miamiplus-florida-test-xc7z045.conf
@@ -1,0 +1,30 @@
+# Florida board common components
+require conf/machine/include/topic-miamiplus.inc
+
+MACHINE_FPGA_BITSTREAM ?= ""
+
+# Extra packages to install for the board
+BOARD_FPGA_PACKAGES ?= "\
+	kernel-module-g-ether \
+	kernel-module-g-mass-storage \
+	kernel-module-g-ncm \
+	kernel-module-gspca-main \
+	kernel-module-u-ether \
+	kernel-module-pwc \
+	kernel-module-usb-f-ecm \
+	kernel-module-usb-f-ecm-subset \
+	kernel-module-usb-f-mass-storage \
+	kernel-module-usb-f-ncm \
+	kernel-module-usb-f-rndis \
+	kernel-module-usbserial \
+	kernel-module-uvcvideo \
+	kernel-module-videobuf2-core \
+	kernel-module-videobuf2-dma-contig \
+	kernel-module-videobuf2-memops \
+	kernel-module-videobuf2-vmalloc \
+	kernel-module-virtio-ring \
+	kernel-module-virtio-rpmsg-bus \
+	kernel-module-virtio \
+	"
+
+MACHINE_FEATURES += "usbhost usbgadget"

commit 4f79ea9f3a380ecaa50306521ec43a3edc996899

linux-topic.bb: Fix wrong revision number
Wrong revision number got into the previous commit.
Fixes: f8c7aaecc1d947e7600b7fea6c471273ce281207

recipes-kernel/linux-zynq/linux-topic.bb
@@ -4,7 +4,7 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
 
 KBRANCH = "topic-miami"
-SRCREV = "bbf9a879a848412520ab263c277acc10a5747845"
+SRCREV = "0d09774ac03397437e8ac65aed5e1482890f24e5"
 LINUX_VERSION = "4.4"
 
 inherit kernel