topic-embedded-products/meta-topic

commit 252f97f214016c61ba6fd23744503a0de23bee66

Start and stop network interfaces using link status
Create ifplugd-auto-net recipe that monitors all network interfaces and starts
them when a link is detected.
Make udhcpd-iface-config RDEPEND on ifplugd-auto-net to automatically start
when needed. Set MTU to 15k on all topic boards.
Simpler init-ifupdown, no need to override it with custom scripts.

recipes-core/images/my-image.bb
@@ -18,6 +18,7 @@ MY_THINGS = "\
 	alsa-utils-aplay alsa-utils-speakertest alsa-utils-amixer alsa-utils-alsactl \
 	i2c-tools \
 	udhcpd-iface-config \
+	iperf3 ethtool tcpdump \
 	"
 
 # Skip packagegroup-base to reduce the number of packages built. Thus, we need

recipes-core/init-ifupdown/init-ifupdown/init
@@ -1,43 +1,0 @@
-#!/bin/sh -e
-### BEGIN INIT INFO
-# Provides:          networking
-# Required-Start:    mountvirtfs $local_fs
-# Required-Stop:     $local_fs
-# Should-Start:      ifupdown
-# Should-Stop:       ifupdown
-# Default-Start:     S
-# Default-Stop:      0 6
-# Short-Description: Raise network interfaces.
-### END INIT INFO
-
-PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-
-case "$1" in
-start)
-	echo -n "Configuring network interfaces... "
-	ifup -a > /dev/null 2>&1 &
-	echo "(background)"
-	;;
-
-stop)
-	echo -n "Deconfiguring network interfaces... "
-	ifdown -a
-	echo "done."
-	;;
-
-force-reload|restart)
-	echo "Running $0 $1 is deprecated because it may not enable again some interfaces"
-	echo "Reconfiguring network interfaces... "
-	ifdown -a || true
-	ifup -a
-	echo "done."
-	;;
-
-*)
-	echo "Usage: /etc/init.d/networking {start|stop}"
-	exit 1
-	;;
-esac
-
-exit 0
-

recipes-core/init-ifupdown/init-ifupdown/topic-miami/interfaces
@@ -11,12 +11,10 @@ iface wlan0 inet dhcp
 	wpa-conf /etc/wpa_supplicant.conf
 
 # Wired interfaces
-auto eth0
 iface eth0 inet dhcp
 iface eth1 inet dhcp
 
 # Ethernet/RNDIS gadget (g_ether)
-auto usb0
 iface usb0 inet static
 	pre-up ifconfig usb0 mtu 15000
 	address 192.168.222.1

recipes-topic/network/ifplugd-auto-net.bb
@@ -1,0 +1,25 @@
+SUMMARY = "Automatically start and stop network interfaces on link status"
+LICENSE = "GPLv2"
+LIC_FILES_CHKSUM = "file://${META_ZYNQ_BASE}/COPYING;md5=751419260aa954499f7abaabaa882bbe"
+PV = "0"
+
+inherit allarch update-rc.d
+
+SRC_URI = "file://init.sh file://ifplugd.auto"
+
+S = "${WORKDIR}"
+PACKAGES = "${PN}"
+FILES_${PN} = "${sysconfdir}"
+
+# Startup similar to init-ifupdown
+INITSCRIPT_NAME = "ifplug-auto-net.sh"
+INITSCRIPT_PARAMS = "start 01 2 3 4 5 ."
+
+do_compile() {
+}
+
+do_install() {
+	install -d ${D}${sysconfdir}/init.d
+	install -m 755 ${S}/init.sh ${D}${sysconfdir}/init.d/${INITSCRIPT_NAME}
+	install -m 755 ${S}/ifplugd.auto ${D}${sysconfdir}/ifplugd.auto
+}

recipes-topic/network/ifplugd-auto-net/ifplugd.auto
@@ -1,0 +1,2 @@
+#!/bin/sh
+exec if$2 $1

recipes-topic/network/ifplugd-auto-net/init.sh
@@ -1,0 +1,30 @@
+#!/bin/sh
+case "$1" in
+start)
+	echo -n "Start ifplugd:"
+	cd /sys/class/net
+	for i in *[0-9]
+	do
+		echo -n " $i"
+		ifplugd -i $i -r /etc/ifplugd.auto
+	done
+	echo "."
+	;;
+
+stop)
+	echo -n "Stop ifplugd:"
+	cd /sys/class/net
+	for i in *[0-9]
+	do
+		echo -n " $i"
+		ifplugd -i $i -k
+	done
+	echo "."
+	;;
+*)
+	echo "Usage: $0 {start|stop}"
+	exit 1
+	;;
+esac
+
+exit 0

recipes-topic/network/udhcpd-iface-config.bb
@@ -3,12 +3,14 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://${META_ZYNQ_BASE}/COPYING;md5=751419260aa954499f7abaabaa882bbe"
 inherit allarch
 PACKAGE_ARCH = "${MACHINE_ARCH}"
-PV = "3"
+PV = "4"
 
 PACKAGES = "${PN}"
 SRC_URI = "file://udhcpd_up.sh file://udhcpd_down.sh file://udhcpd.*.conf"
 S = "${WORKDIR}"
 FILES_${PN} = "${sysconfdir}"
+
+RDEPENDS_${PN} = "ifplugd-auto-net"
 
 do_compile() {
 }

recipes-topic/network/udhcpd-iface-config/topic-miami/udhcpd.usb0.conf
@@ -4,4 +4,5 @@ lease_file	/var/run/udhcpd.usb0.leases
 start	192.168.222.2
 end	192.168.222.7
 option	subnet 255.255.255.240
+option	mtu 15000
 remaining	no

commit f652e7f18a0a09016599124d70aba7b7f4f4c6fd

my-image: Remove ethernet tools
Commit 252f97f accidentally added network diagnostic tools to the
image, adding a dependency on meta-networking. Revert that change.

Fixes: 252f97f214016c61ba6fd23744503a0de23bee66

recipes-core/images/my-image.bb
@@ -18,7 +18,6 @@ MY_THINGS = "\
 	alsa-utils-aplay alsa-utils-speakertest alsa-utils-amixer alsa-utils-alsactl \
 	i2c-tools \
 	udhcpd-iface-config \
-	iperf3 ethtool tcpdump \
 	"
 
 # Skip packagegroup-base to reduce the number of packages built. Thus, we need