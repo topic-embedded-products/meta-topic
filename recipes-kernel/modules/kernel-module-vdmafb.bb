topic-embedded-products/meta-topic

commit 8712d17dead08f1679701023fc2a348b07abd7af

Upgrade Linux kernel to 4.6

recipes-kernel/modules/kernel-module-vdmafb.bb
@@ -1,7 +1,7 @@
 SUMMARY = "Kernel driver module VDMA video driver"
 require kernel-module-topic.inc
 
-SRCREV = "8ec9c7da053e785ad09d3e20d945600d011c38b7"
+SRCREV = "${AUTOREV}"
 
 RRECOMMENDS_${PN} = "\
 	kernel-module-backlight \

recipes-kernel/linux-zynq/linux-topic.bb
@@ -4,8 +4,8 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
 
 KBRANCH = "topic-miami"
-SRCREV = "d2feddc9eb4364fe38d62b1cbc68768b60f948b9"
-LINUX_VERSION = "4.4"
+SRCREV = "151f49acc68a52965f7066656e4e9236f54d0396"
+LINUX_VERSION = "4.6"
 
 inherit kernel
 require recipes-kernel/linux/linux-dtb.inc
@@ -25,7 +25,8 @@ SRC_URI = "\
 	${KERNEL_GIT_REPO};branch=${KBRANCH} \
 	file://defconfig \
 	file://0001-hwmon-max6650.c-Add-devicetree-support.patch \
-	file://0002-hwmon-max6650-Allow-fan-shutdown-and-a-more-intuitiv.patch \
+	file://0002-hwmon-max6650.c-Add-devicetree-support-documentation.patch \
+	file://0003-hwmon-max6650-Allow-fan-shutdown-and-initial-rpm-tar.patch \
 	"
 
 KERNEL_IMAGETYPE = "uImage"
@@ -35,6 +36,7 @@ KERNEL_DEVICETREE_topic-miami = "\
 	topic-miami-florida-gen.dtb \
 	topic-miami-florida-gen-pt.dtb \
 	topic-miami-florida-gen-nand.dtb \
+	topic-miami-florida-gen-amp.dtb \
 	topic-miami-florida-mio.dtb \
 	topic-miami-florida-mio-dyplo.dtb \
 	topic-miami-florida-mio-nand-dyplo.dtb \

recipes-kernel/linux-zynq/linux-topic/0001-hwmon-max6650.c-Add-devicetree-support.patch
@@ -1,7 +1,7 @@
-From e4d7b3d775ca024c4a348e5ada303a7961889e6a Mon Sep 17 00:00:00 2001
+From 6975beb758a16a7bbb0056d554d4b68db8d4422c Mon Sep 17 00:00:00 2001
 From: Mike Looijmans <mike.looijmans@topic.nl>
 Date: Tue, 9 Aug 2016 08:24:06 +0200
-Subject: [PATCH 1/2] hwmon/max6650.c: Add devicetree support
+Subject: [PATCH 1/3] hwmon: (max6650.c) Add devicetree support
 
 Parse devicetree parameters for voltage and prescaler setting. This allows
 using multiple max6550 devices with varying settings, and also makes it
@@ -9,50 +9,22 @@ possible to instantiate and configure the device using devicetree.
 
 Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
 ---
- .../devicetree/bindings/hwmon/max6650.txt          | 20 ++++++++++++++++
- drivers/hwmon/max6650.c                            | 28 +++++++++++++---------
- 2 files changed, 37 insertions(+), 11 deletions(-)
- create mode 100644 Documentation/devicetree/bindings/hwmon/max6650.txt
+ drivers/hwmon/max6650.c | 47 ++++++++++++++++++++++++++++++++++++-----------
+ 1 file changed, 36 insertions(+), 11 deletions(-)
 
-diff --git a/Documentation/devicetree/bindings/hwmon/max6650.txt b/Documentation/devicetree/bindings/hwmon/max6650.txt
-new file mode 100644
-index 0000000..89d87c6
---- /dev/null
-+++ b/Documentation/devicetree/bindings/hwmon/max6650.txt
-@@ -0,0 +1,20 @@
-+Bindings for MAX6651 and MAX6650 I2C fan controllers
-+
-+Required properties:
-+- compatible : One of "max6650" or "max6651"
-+- reg        : I2C address
-+
-+Optional properties:
-+- fan-voltage    : The supply voltage of the fan. Valid values are 5 and 12.
-+                   Default is to use the chip's current setting.
-+- fan-prescale   : Pre-scaling value as per datasheet. Valid are 1, 2, 4, 8, 16.
-+                   Default is to use the chip's current setting.
-+ 
-+
-+Example:
-+	fan-max6650: max6650@1b {
-+		reg = <0x1b>;
-+		compatible = "max6650";
-+		fan-voltage = <12>;
-+		fan-prescale = <4>;
-+	};
 diff --git a/drivers/hwmon/max6650.c b/drivers/hwmon/max6650.c
-index 162a520..56a6c87 100644
+index 162a520..c87517a 100644
 --- a/drivers/hwmon/max6650.c
 +++ b/drivers/hwmon/max6650.c
-@@ -41,14 +41,14 @@
+@@ -39,6 +39,7 @@
+ #include <linux/hwmon.h>
+ #include <linux/hwmon-sysfs.h>
  #include <linux/err.h>
++#include <linux/of_device.h>
  
  /*
-- * Insmod parameters
-+ * Insmod parameters (for backward compatibility)
-  */
- 
- /* fan_voltage: 5=5V fan, 12=12V fan, 0=don't change */
+  * Insmod parameters
+@@ -48,7 +49,7 @@
  static int fan_voltage;
  /* prescaler: Possible values are 1, 2, 4, 8, 16 or 0 for don't change */
  static int prescaler;
@@ -61,23 +33,45 @@ index 162a520..56a6c87 100644
  static int clock = 254000;
  
  module_param(fan_voltage, int, S_IRUGO);
-@@ -566,6 +566,15 @@ static int max6650_init_client(struct max6650_data *data,
+@@ -133,6 +134,19 @@ static const u8 tach_reg[] = {
+ 	MAX6650_REG_TACH3,
+ };
+ 
++static const struct of_device_id max6650_dt_match[] = {
++	{
++		.compatible = "maxim,max6650",
++		.data = (void *)1
++	},
++	{
++		.compatible = "maxim,max6651",
++		.data = (void *)4
++	},
++	{ },
++};
++MODULE_DEVICE_TABLE(of, max6650_dt_match);
++
+ static struct max6650_data *max6650_update_device(struct device *dev)
+ {
+ 	struct max6650_data *data = dev_get_drvdata(dev);
+@@ -566,6 +580,17 @@ static int max6650_init_client(struct max6650_data *data,
  	struct device *dev = &client->dev;
  	int config;
  	int err = -EIO;
 +	u32 voltage;
 +	u32 prescale;
 +
-+	if (of_property_read_u32(client->dev.of_node, "fan-voltage",
++	if (of_property_read_u32(dev->of_node, "maxim,fan-microvolt",
 +				 &voltage))
 +		voltage = fan_voltage;
-+	if (of_property_read_u32(client->dev.of_node, "fan-prescale",
++	else
++		voltage /= 1000000; /* Microvolts to volts */
++	if (of_property_read_u32(dev->of_node, "maxim,fan-prescale",
 +				 &prescale))
 +		prescale = prescaler;
  
  	config = i2c_smbus_read_byte_data(client, MAX6650_REG_CONFIG);
  
-@@ -574,7 +583,7 @@ static int max6650_init_client(struct max6650_data *data,
+@@ -574,7 +599,7 @@ static int max6650_init_client(struct max6650_data *data,
  		return err;
  	}
  
@@ -86,7 +80,7 @@ index 162a520..56a6c87 100644
  	case 0:
  		break;
  	case 5:
-@@ -584,14 +593,10 @@ static int max6650_init_client(struct max6650_data *data,
+@@ -584,14 +609,10 @@ static int max6650_init_client(struct max6650_data *data,
  		config |= MAX6650_CFG_V12;
  		break;
  	default:
@@ -103,7 +97,7 @@ index 162a520..56a6c87 100644
  	case 0:
  		break;
  	case 1:
-@@ -614,10 +619,11 @@ static int max6650_init_client(struct max6650_data *data,
+@@ -614,10 +635,11 @@ static int max6650_init_client(struct max6650_data *data,
  			 | MAX6650_CFG_PRESCALER_16;
  		break;
  	default:
@@ -117,6 +111,32 @@ index 162a520..56a6c87 100644
  		 1 << (config & MAX6650_CFG_PRESCALER_MASK));
  
  	/*
+@@ -651,6 +673,8 @@ static int max6650_probe(struct i2c_client *client,
+ 			 const struct i2c_device_id *id)
+ {
+ 	struct device *dev = &client->dev;
++	const struct of_device_id *of_id =
++		of_match_device(of_match_ptr(max6650_dt_match), dev);
+ 	struct max6650_data *data;
+ 	struct device *hwmon_dev;
+ 	int err;
+@@ -661,7 +685,7 @@ static int max6650_probe(struct i2c_client *client,
+ 
+ 	data->client = client;
+ 	mutex_init(&data->update_lock);
+-	data->nr_fans = id->driver_data;
++	data->nr_fans = of_id ? (int)(uintptr_t)of_id->data : id->driver_data;
+ 
+ 	/*
+ 	 * Initialize the max6650 chip
+@@ -691,6 +715,7 @@ MODULE_DEVICE_TABLE(i2c, max6650_id);
+ static struct i2c_driver max6650_driver = {
+ 	.driver = {
+ 		.name	= "max6650",
++		.of_match_table = of_match_ptr(max6650_dt_match),
+ 	},
+ 	.probe		= max6650_probe,
+ 	.id_table	= max6650_id,
 -- 
 1.9.1
 

recipes-kernel/linux-zynq/linux-topic/0002-hwmon-max6650-Allow-fan-shutdown-and-a-more-intuitiv.patch
@@ -1,329 +1,0 @@
-From 70f8d80784e9d373b577b81a92344595b4d1bedc Mon Sep 17 00:00:00 2001
-From: Mike Looijmans <mike.looijmans@topic.nl>
-Date: Wed, 10 Aug 2016 14:48:11 +0200
-Subject: [PATCH 2/2] hwmon/max6650: Allow fan shutdown and a more intuitive
- mode  interface
-
-The fan can be stopped by writing "0" to fan1_target in sysfs.
-
-Writing non-zero values to fan1_target or pwm1 in sysfs automatically
-selects the corresponding control mode (closed or open loop).
-
-This allows userspace applications to control the fan speed without
-the need to know specific details of the controller (like the fact
-that fan1_target does not take effect when pwm1_enable is set to
-anything but "2").
-
-Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
----
- .../devicetree/bindings/hwmon/max6650.txt          |  16 ++-
- Documentation/hwmon/max6650                        |   5 +-
- drivers/hwmon/max6650.c                            | 151 +++++++++++++--------
- 3 files changed, 111 insertions(+), 61 deletions(-)
-
-diff --git a/Documentation/devicetree/bindings/hwmon/max6650.txt b/Documentation/devicetree/bindings/hwmon/max6650.txt
-index 89d87c6..9bc0b6f 100644
---- a/Documentation/devicetree/bindings/hwmon/max6650.txt
-+++ b/Documentation/devicetree/bindings/hwmon/max6650.txt
-@@ -1,15 +1,22 @@
- Bindings for MAX6651 and MAX6650 I2C fan controllers
- 
-+Reference:
-+[1] https://datasheets.maximintegrated.com/en/ds/MAX6650-MAX6651.pdf
-+
- Required properties:
--- compatible : One of "max6650" or "max6651"
--- reg        : I2C address
-+- compatible : Either "max6650" or "max6651"
-+- reg        : I2C address, one of 0x1b, 0x1f, 0x4b, 0x48
- 
- Optional properties:
- - fan-voltage    : The supply voltage of the fan. Valid values are 5 and 12.
-                    Default is to use the chip's current setting.
--- fan-prescale   : Pre-scaling value as per datasheet. Valid are 1, 2, 4, 8, 16.
-+- fan-prescale   : Pre-scaling value as per datasheet [1], depends on fan type.
-+                   Valid values are 1, 2, 4, 8, 16.
-                    Default is to use the chip's current setting.
-- 
-+- fan-target-rpm : Initial requested fan rotation speed. If specified, the
-+                   driver selects closed-loop mode and configures the speed.
-+                   This ensures the fan is already running before userspace
-+                   takes over.
- 
- Example:
- 	fan-max6650: max6650@1b {
-@@ -17,4 +24,5 @@ Example:
- 		compatible = "max6650";
- 		fan-voltage = <12>;
- 		fan-prescale = <4>;
-+		fan-target-rpm = <1200>;
- 	};
-diff --git a/Documentation/hwmon/max6650 b/Documentation/hwmon/max6650
-index 58d9644..53e308b 100644
---- a/Documentation/hwmon/max6650
-+++ b/Documentation/hwmon/max6650
-@@ -33,9 +33,12 @@ fan2_input	ro	"
- fan3_input	ro	"
- fan4_input	ro	"
- fan1_target	rw	desired fan speed in RPM (closed loop mode only)
-+			Set to 0 to stop the fan. Writing any other value sets
-+			the regulator mode to "closed loop".
- pwm1_enable	rw	regulator mode, 0=full on, 1=open loop, 2=closed loop
- pwm1		rw	relative speed (0-255), 255=max. speed.
--			Used in open loop mode only.
-+			Set to 0 to stop the fan. Writing any other value sets
-+			the regulator mode to "open loop".
- fan1_div	rw	sets the speed range the inputs can handle. Legal
- 			values are 1, 2, 4, and 8. Use lower values for
- 			faster fans.
-diff --git a/drivers/hwmon/max6650.c b/drivers/hwmon/max6650.c
-index 56a6c87..2c7b0ef 100644
---- a/drivers/hwmon/max6650.c
-+++ b/drivers/hwmon/max6650.c
-@@ -171,6 +171,33 @@ static struct max6650_data *max6650_update_device(struct device *dev)
- 	return data;
- }
- 
-+static bool max6650_is_powerdown(const struct max6650_data *data)
-+{
-+	return (data->config & MAX6650_CFG_MODE_MASK) == MAX6650_CFG_MODE_OFF;
-+}
-+
-+/*
-+ * Change the operating mode of the chip (if needed).
-+ * mode is one of the MAX6650_CFG_MODE_* values.
-+ */
-+static int max6650_set_operating_mode(struct max6650_data *data, u8 mode)
-+{
-+	int result;
-+	u8 config = data->config;
-+
-+	if (mode == (config & MAX6650_CFG_MODE_MASK))
-+		return 0;
-+
-+	config = (config & ~MAX6650_CFG_MODE_MASK) | mode;
-+
-+	result = i2c_smbus_write_byte_data(data->client, MAX6650_REG_CONFIG,
-+					   config);
-+	if (result < 0)
-+		return result;
-+
-+	data->config = config;
-+}
-+
- static ssize_t get_fan(struct device *dev, struct device_attribute *devattr,
- 		       char *buf)
- {
-@@ -244,26 +271,26 @@ static ssize_t get_target(struct device *dev, struct device_attribute *devattr,
- 	 *    FanSpeed = KSCALE x fCLK / [256 x (KTACH + 1)]
- 	 *
- 	 * then multiply by 60 to give rpm.
-+	 *
-+	 * When not running, report target to be "0".
- 	 */
- 
--	kscale = DIV_FROM_REG(data->config);
--	ktach = data->speed;
--	rpm = 60 * kscale * clock / (256 * (ktach + 1));
-+	if (max6650_is_powerdown(data))
-+		rpm = 0;
-+	else {
-+		kscale = DIV_FROM_REG(data->config);
-+		ktach = data->speed;
-+		rpm = 60 * kscale * clock / (256 * (ktach + 1));
-+	}
- 	return sprintf(buf, "%d\n", rpm);
- }
- 
--static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
--			 const char *buf, size_t count)
-+static int max6650_set_target(struct max6650_data *data, unsigned long rpm)
- {
--	struct max6650_data *data = dev_get_drvdata(dev);
--	struct i2c_client *client = data->client;
- 	int kscale, ktach;
--	unsigned long rpm;
--	int err;
- 
--	err = kstrtoul(buf, 10, &rpm);
--	if (err)
--		return err;
-+	if (rpm == 0)
-+		return max6650_set_operating_mode(data, MAX6650_CFG_MODE_OFF);
- 
- 	rpm = clamp_val(rpm, FAN_RPM_MIN, FAN_RPM_MAX);
- 
-@@ -274,8 +301,6 @@ static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
- 	 *     KTACH = [(fCLK x KSCALE) / (256 x FanSpeed)] - 1
- 	 */
- 
--	mutex_lock(&data->update_lock);
--
- 	kscale = DIV_FROM_REG(data->config);
- 	ktach = ((clock * kscale) / (256 * rpm / 60)) - 1;
- 	if (ktach < 0)
-@@ -284,7 +309,25 @@ static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
- 		ktach = 255;
- 	data->speed = ktach;
- 
--	i2c_smbus_write_byte_data(client, MAX6650_REG_SPEED, data->speed);
-+	i2c_smbus_write_byte_data(data->client, MAX6650_REG_SPEED, data->speed);
-+
-+	return max6650_set_operating_mode(data, MAX6650_CFG_MODE_CLOSED_LOOP);
-+}
-+
-+static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
-+			 const char *buf, size_t count)
-+{
-+	struct max6650_data *data = dev_get_drvdata(dev);
-+	unsigned long rpm;
-+	int err;
-+
-+	err = kstrtoul(buf, 10, &rpm);
-+	if (err)
-+		return err;
-+
-+	mutex_lock(&data->update_lock);
-+
-+	err = max6650_set_target(data, rpm);
- 
- 	mutex_unlock(&data->update_lock);
- 
-@@ -306,17 +349,21 @@ static ssize_t get_pwm(struct device *dev, struct device_attribute *devattr,
- 	int pwm;
- 	struct max6650_data *data = max6650_update_device(dev);
- 
--	/*
--	 * Useful range for dac is 0-180 for 12V fans and 0-76 for 5V fans.
--	 * Lower DAC values mean higher speeds.
--	 */
--	if (data->config & MAX6650_CFG_V12)
--		pwm = 255 - (255 * (int)data->dac)/180;
--	else
--		pwm = 255 - (255 * (int)data->dac)/76;
--
--	if (pwm < 0)
-+	if (max6650_is_powerdown(data))
- 		pwm = 0;
-+	else {
-+		/*
-+		 * Useful range for dac is 0-180 for 12V fans and 0-76 for 5V
-+		 * fans. Lower DAC values mean higher speeds.
-+		 */
-+		if (data->config & MAX6650_CFG_V12)
-+			pwm = 255 - (255 * (int)data->dac)/180;
-+		else
-+			pwm = 255 - (255 * (int)data->dac)/76;
-+
-+		if (pwm < 0)
-+			pwm = 0;
-+	}
- 
- 	return sprintf(buf, "%d\n", pwm);
- }
-@@ -337,16 +384,20 @@ static ssize_t set_pwm(struct device *dev, struct device_attribute *devattr,
- 
- 	mutex_lock(&data->update_lock);
- 
--	if (data->config & MAX6650_CFG_V12)
--		data->dac = 180 - (180 * pwm)/255;
--	else
--		data->dac = 76 - (76 * pwm)/255;
--
--	i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, data->dac);
-+	if (pwm == 0)
-+		err = max6650_set_operating_mode(data, MAX6650_CFG_MODE_OFF);
-+	else {
-+		if (data->config & MAX6650_CFG_V12)
-+			data->dac = 180 - (180 * pwm)/255;
-+		else
-+			data->dac = 76 - (76 * pwm)/255;
-+		i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, data->dac);
-+		err = max6650_set_operating_mode(data, MAX6650_CFG_MODE_OPEN_LOOP);
-+	}
- 
- 	mutex_unlock(&data->update_lock);
- 
--	return count;
-+	return err < 0 ? err : count;
- }
- 
- /*
-@@ -371,25 +422,24 @@ static ssize_t set_enable(struct device *dev, struct device_attribute *devattr,
- 			  const char *buf, size_t count)
- {
- 	struct max6650_data *data = dev_get_drvdata(dev);
--	struct i2c_client *client = data->client;
--	int max6650_modes[3] = {0, 3, 2};
- 	unsigned long mode;
- 	int err;
-+	const u8 max6650_modes[3] = {
-+		MAX6650_CFG_MODE_ON,
-+		MAX6650_CFG_MODE_OPEN_LOOP,
-+		MAX6650_CFG_MODE_CLOSED_LOOP
-+		};
- 
- 	err = kstrtoul(buf, 10, &mode);
- 	if (err)
- 		return err;
- 
--	if (mode > 2)
-+	if (mode >= ARRAY_SIZE(max6650_modes))
- 		return -EINVAL;
- 
- 	mutex_lock(&data->update_lock);
- 
--	data->config = i2c_smbus_read_byte_data(client, MAX6650_REG_CONFIG);
--	data->config = (data->config & ~MAX6650_CFG_MODE_MASK)
--		       | (max6650_modes[mode] << 4);
--
--	i2c_smbus_write_byte_data(client, MAX6650_REG_CONFIG, data->config);
-+	max6650_set_operating_mode(data, max6650_modes[mode]);
- 
- 	mutex_unlock(&data->update_lock);
- 
-@@ -568,6 +618,7 @@ static int max6650_init_client(struct max6650_data *data,
- 	int err = -EIO;
- 	u32 voltage;
- 	u32 prescale;
-+	u32 target_rpm;
- 
- 	if (of_property_read_u32(client->dev.of_node, "fan-voltage",
- 				 &voltage))
-@@ -626,22 +677,6 @@ static int max6650_init_client(struct max6650_data *data,
- 		 (config & MAX6650_CFG_V12) ? 12 : 5,
- 		 1 << (config & MAX6650_CFG_PRESCALER_MASK));
- 
--	/*
--	 * If mode is set to "full off", we change it to "open loop" and
--	 * set DAC to 255, which has the same effect. We do this because
--	 * there's no "full off" mode defined in hwmon specifications.
--	 */
--
--	if ((config & MAX6650_CFG_MODE_MASK) == MAX6650_CFG_MODE_OFF) {
--		dev_dbg(dev, "Change mode to open loop, full off.\n");
--		config = (config & ~MAX6650_CFG_MODE_MASK)
--			 | MAX6650_CFG_MODE_OPEN_LOOP;
--		if (i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, 255)) {
--			dev_err(dev, "DAC write error, aborting.\n");
--			return err;
--		}
--	}
--
- 	if (i2c_smbus_write_byte_data(client, MAX6650_REG_CONFIG, config)) {
- 		dev_err(dev, "Config write error, aborting.\n");
- 		return err;
-@@ -650,6 +685,10 @@ static int max6650_init_client(struct max6650_data *data,
- 	data->config = config;
- 	data->count = i2c_smbus_read_byte_data(client, MAX6650_REG_COUNT);
- 
-+	if (!of_property_read_u32(client->dev.of_node, "fan-target-rpm",
-+				 &target_rpm))
-+		max6650_set_target(data, target_rpm);
-+
- 	return 0;
- }
- 
--- 
-1.9.1
-

recipes-kernel/linux-zynq/linux-topic/0002-hwmon-max6650.c-Add-devicetree-support-documentation.patch
@@ -1,0 +1,46 @@
+From 337735c9359e8bab6a66a21b405481d75c5f96f0 Mon Sep 17 00:00:00 2001
+From: Mike Looijmans <mike.looijmans@topic.nl>
+Date: Tue, 16 Aug 2016 09:09:05 +0200
+Subject: [PATCH 2/3] hwmon: (max6650.c) Add devicetree support documentation
+
+This adds the devicetree binding documentation for the max6650 fan
+controller.
+
+Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
+---
+ .../devicetree/bindings/hwmon/max6650.txt          | 23 ++++++++++++++++++++++
+ 1 file changed, 23 insertions(+)
+ create mode 100644 Documentation/devicetree/bindings/hwmon/max6650.txt
+
+diff --git a/Documentation/devicetree/bindings/hwmon/max6650.txt b/Documentation/devicetree/bindings/hwmon/max6650.txt
+new file mode 100644
+index 0000000..d6c10e3
+--- /dev/null
++++ b/Documentation/devicetree/bindings/hwmon/max6650.txt
+@@ -0,0 +1,23 @@
++Bindings for MAX6651 and MAX6650 I2C fan controllers
++
++Reference:
++[1]	https://datasheets.maximintegrated.com/en/ds/MAX6650-MAX6651.pdf
++
++Required properties:
++- compatible : One of "maxim,max6650" or "maxim,max6651"
++- reg        : I2C address, one of 0x1b, 0x1f, 0x4b, 0x48.
++
++Optional properties, default is to retain the chip's current setting:
++- maxim,fan-microvolt : The supply voltage of the fan, either 5000000 uV or
++			12000000 uV.
++- maxim,fan-prescale  : Pre-scaling value, as per datasheet [1]. Lower values
++			allow more fine-grained control of slower fans.
++			Valid: 1, 2, 4, 8, 16.
++
++Example:
++	fan-max6650: max6650@1b {
++		reg = <0x1b>;
++		compatible = "maxim,max6650";
++		maxim,fan-microvolt = <12000000>;
++		maxim,fan-prescale = <4>;
++	};
+-- 
+1.9.1
+

recipes-kernel/linux-zynq/linux-topic/0003-hwmon-max6650-Allow-fan-shutdown-and-initial-rpm-tar.patch
@@ -1,0 +1,277 @@
+From 2457a0492ed4f852a13cda7b2a56173947c66e12 Mon Sep 17 00:00:00 2001
+From: Mike Looijmans <mike.looijmans@topic.nl>
+Date: Wed, 10 Aug 2016 14:48:11 +0200
+Subject: [PATCH 3/3] hwmon: (max6650) Allow fan shutdown and initial rpm
+ target
+
+The fan can be stopped by writing "3" to pwm1_enable in sysfs.
+
+Add devicetree property for early initialization of the fan controller
+to prevent overheating, for example when resetting the board while the
+fan was completely turned off.
+
+Also improve error reporting, I2C failures were ignored while writing
+new values.
+
+Signed-off-by: Mike Looijmans <mike.looijmans@topic.nl>
+---
+ .../devicetree/bindings/hwmon/max6650.txt          |   5 +
+ Documentation/hwmon/max6650                        |   1 +
+ drivers/hwmon/max6650.c                            | 112 +++++++++++++--------
+ 3 files changed, 75 insertions(+), 43 deletions(-)
+
+diff --git a/Documentation/devicetree/bindings/hwmon/max6650.txt b/Documentation/devicetree/bindings/hwmon/max6650.txt
+index d6c10e3..f6bd87d 100644
+--- a/Documentation/devicetree/bindings/hwmon/max6650.txt
++++ b/Documentation/devicetree/bindings/hwmon/max6650.txt
+@@ -13,6 +13,10 @@ Optional properties, default is to retain the chip's current setting:
+ - maxim,fan-prescale  : Pre-scaling value, as per datasheet [1]. Lower values
+ 			allow more fine-grained control of slower fans.
+ 			Valid: 1, 2, 4, 8, 16.
++- maxim,fan-target-rpm: Initial requested fan rotation speed. If specified, the
++			driver selects closed-loop mode and the requested speed.
++			This ensures the fan is already running before userspace
++			takes over.
+ 
+ Example:
+ 	fan-max6650: max6650@1b {
+@@ -20,4 +24,5 @@ Example:
+ 		compatible = "maxim,max6650";
+ 		maxim,fan-microvolt = <12000000>;
+ 		maxim,fan-prescale = <4>;
++		maxim,fan-target-rpm = <1200>;
+ 	};
+diff --git a/Documentation/hwmon/max6650 b/Documentation/hwmon/max6650
+index 58d9644..dff1d29 100644
+--- a/Documentation/hwmon/max6650
++++ b/Documentation/hwmon/max6650
+@@ -34,6 +34,7 @@ fan3_input	ro	"
+ fan4_input	ro	"
+ fan1_target	rw	desired fan speed in RPM (closed loop mode only)
+ pwm1_enable	rw	regulator mode, 0=full on, 1=open loop, 2=closed loop
++			3=off
+ pwm1		rw	relative speed (0-255), 255=max. speed.
+ 			Used in open loop mode only.
+ fan1_div	rw	sets the speed range the inputs can handle. Legal
+diff --git a/drivers/hwmon/max6650.c b/drivers/hwmon/max6650.c
+index c87517a..c0bc225 100644
+--- a/drivers/hwmon/max6650.c
++++ b/drivers/hwmon/max6650.c
+@@ -185,6 +185,30 @@ static struct max6650_data *max6650_update_device(struct device *dev)
+ 	return data;
+ }
+ 
++/*
++ * Change the operating mode of the chip (if needed).
++ * mode is one of the MAX6650_CFG_MODE_* values.
++ */
++static int max6650_set_operating_mode(struct max6650_data *data, u8 mode)
++{
++	int result;
++	u8 config = data->config;
++
++	if (mode == (config & MAX6650_CFG_MODE_MASK))
++		return 0;
++
++	config = (config & ~MAX6650_CFG_MODE_MASK) | mode;
++
++	result = i2c_smbus_write_byte_data(data->client, MAX6650_REG_CONFIG,
++					   config);
++	if (result < 0)
++		return result;
++
++	data->config = config;
++
++	return 0;
++}
++
+ static ssize_t get_fan(struct device *dev, struct device_attribute *devattr,
+ 		       char *buf)
+ {
+@@ -266,18 +290,12 @@ static ssize_t get_target(struct device *dev, struct device_attribute *devattr,
+ 	return sprintf(buf, "%d\n", rpm);
+ }
+ 
+-static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
+-			 const char *buf, size_t count)
++static int max6650_set_target(struct max6650_data *data, unsigned long rpm)
+ {
+-	struct max6650_data *data = dev_get_drvdata(dev);
+-	struct i2c_client *client = data->client;
+ 	int kscale, ktach;
+-	unsigned long rpm;
+-	int err;
+ 
+-	err = kstrtoul(buf, 10, &rpm);
+-	if (err)
+-		return err;
++	if (rpm == 0)
++		return max6650_set_operating_mode(data, MAX6650_CFG_MODE_OFF);
+ 
+ 	rpm = clamp_val(rpm, FAN_RPM_MIN, FAN_RPM_MAX);
+ 
+@@ -288,8 +306,6 @@ static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
+ 	 *     KTACH = [(fCLK x KSCALE) / (256 x FanSpeed)] - 1
+ 	 */
+ 
+-	mutex_lock(&data->update_lock);
+-
+ 	kscale = DIV_FROM_REG(data->config);
+ 	ktach = ((clock * kscale) / (256 * rpm / 60)) - 1;
+ 	if (ktach < 0)
+@@ -298,10 +314,30 @@ static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
+ 		ktach = 255;
+ 	data->speed = ktach;
+ 
+-	i2c_smbus_write_byte_data(client, MAX6650_REG_SPEED, data->speed);
++	return i2c_smbus_write_byte_data(data->client, MAX6650_REG_SPEED,
++					 data->speed);
++}
++
++static ssize_t set_target(struct device *dev, struct device_attribute *devattr,
++			 const char *buf, size_t count)
++{
++	struct max6650_data *data = dev_get_drvdata(dev);
++	unsigned long rpm;
++	int err;
++
++	err = kstrtoul(buf, 10, &rpm);
++	if (err)
++		return err;
++
++	mutex_lock(&data->update_lock);
++
++	err = max6650_set_target(data, rpm);
+ 
+ 	mutex_unlock(&data->update_lock);
+ 
++	if (err < 0)
++		return err;
++
+ 	return count;
+ }
+ 
+@@ -321,8 +357,8 @@ static ssize_t get_pwm(struct device *dev, struct device_attribute *devattr,
+ 	struct max6650_data *data = max6650_update_device(dev);
+ 
+ 	/*
+-	 * Useful range for dac is 0-180 for 12V fans and 0-76 for 5V fans.
+-	 * Lower DAC values mean higher speeds.
++	 * Useful range for dac is 0-180 for 12V fans and 0-76 for 5V
++	 * fans. Lower DAC values mean higher speeds.
+ 	 */
+ 	if (data->config & MAX6650_CFG_V12)
+ 		pwm = 255 - (255 * (int)data->dac)/180;
+@@ -355,12 +391,11 @@ static ssize_t set_pwm(struct device *dev, struct device_attribute *devattr,
+ 		data->dac = 180 - (180 * pwm)/255;
+ 	else
+ 		data->dac = 76 - (76 * pwm)/255;
+-
+-	i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, data->dac);
++	err = i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, data->dac);
+ 
+ 	mutex_unlock(&data->update_lock);
+ 
+-	return count;
++	return err < 0 ? err : count;
+ }
+ 
+ /*
+@@ -369,14 +404,14 @@ static ssize_t set_pwm(struct device *dev, struct device_attribute *devattr,
+  * 0 = Fan always on
+  * 1 = Open loop, Voltage is set according to speed, not regulated.
+  * 2 = Closed loop, RPM for all fans regulated by fan1 tachometer
++ * 3 = Fan off
+  */
+-
+ static ssize_t get_enable(struct device *dev, struct device_attribute *devattr,
+ 			  char *buf)
+ {
+ 	struct max6650_data *data = max6650_update_device(dev);
+ 	int mode = (data->config & MAX6650_CFG_MODE_MASK) >> 4;
+-	int sysfs_modes[4] = {0, 1, 2, 1};
++	int sysfs_modes[4] = {0, 3, 2, 1};
+ 
+ 	return sprintf(buf, "%d\n", sysfs_modes[mode]);
+ }
+@@ -385,25 +420,25 @@ static ssize_t set_enable(struct device *dev, struct device_attribute *devattr,
+ 			  const char *buf, size_t count)
+ {
+ 	struct max6650_data *data = dev_get_drvdata(dev);
+-	struct i2c_client *client = data->client;
+-	int max6650_modes[3] = {0, 3, 2};
+ 	unsigned long mode;
+ 	int err;
++	const u8 max6650_modes[] = {
++		MAX6650_CFG_MODE_ON,
++		MAX6650_CFG_MODE_OPEN_LOOP,
++		MAX6650_CFG_MODE_CLOSED_LOOP,
++		MAX6650_CFG_MODE_OFF,
++		};
+ 
+ 	err = kstrtoul(buf, 10, &mode);
+ 	if (err)
+ 		return err;
+ 
+-	if (mode > 2)
++	if (mode >= ARRAY_SIZE(max6650_modes))
+ 		return -EINVAL;
+ 
+ 	mutex_lock(&data->update_lock);
+ 
+-	data->config = i2c_smbus_read_byte_data(client, MAX6650_REG_CONFIG);
+-	data->config = (data->config & ~MAX6650_CFG_MODE_MASK)
+-		       | (max6650_modes[mode] << 4);
+-
+-	i2c_smbus_write_byte_data(client, MAX6650_REG_CONFIG, data->config);
++	max6650_set_operating_mode(data, max6650_modes[mode]);
+ 
+ 	mutex_unlock(&data->update_lock);
+ 
+@@ -582,6 +617,7 @@ static int max6650_init_client(struct max6650_data *data,
+ 	int err = -EIO;
+ 	u32 voltage;
+ 	u32 prescale;
++	u32 target_rpm;
+ 
+ 	if (of_property_read_u32(dev->of_node, "maxim,fan-microvolt",
+ 				 &voltage))
+@@ -642,22 +678,6 @@ static int max6650_init_client(struct max6650_data *data,
+ 		 (config & MAX6650_CFG_V12) ? 12 : 5,
+ 		 1 << (config & MAX6650_CFG_PRESCALER_MASK));
+ 
+-	/*
+-	 * If mode is set to "full off", we change it to "open loop" and
+-	 * set DAC to 255, which has the same effect. We do this because
+-	 * there's no "full off" mode defined in hwmon specifications.
+-	 */
+-
+-	if ((config & MAX6650_CFG_MODE_MASK) == MAX6650_CFG_MODE_OFF) {
+-		dev_dbg(dev, "Change mode to open loop, full off.\n");
+-		config = (config & ~MAX6650_CFG_MODE_MASK)
+-			 | MAX6650_CFG_MODE_OPEN_LOOP;
+-		if (i2c_smbus_write_byte_data(client, MAX6650_REG_DAC, 255)) {
+-			dev_err(dev, "DAC write error, aborting.\n");
+-			return err;
+-		}
+-	}
+-
+ 	if (i2c_smbus_write_byte_data(client, MAX6650_REG_CONFIG, config)) {
+ 		dev_err(dev, "Config write error, aborting.\n");
+ 		return err;
+@@ -666,6 +686,12 @@ static int max6650_init_client(struct max6650_data *data,
+ 	data->config = config;
+ 	data->count = i2c_smbus_read_byte_data(client, MAX6650_REG_COUNT);
+ 
++	if (!of_property_read_u32(client->dev.of_node, "maxim,fan-target-rpm",
++				  &target_rpm)) {
++		max6650_set_target(data, target_rpm);
++		max6650_set_operating_mode(data, MAX6650_CFG_MODE_CLOSED_LOOP);
++	}
++
+ 	return 0;
+ }
+ 
+-- 
+1.9.1
+

commit 3e49be42a51c0378bf67efe4f1544fd35a925f08

kernel-module-vdmafb: Remove AUTOREV
Accidentally still had SRCREV=${AUTOREV} causing git access during parse.

Fixes: 8712d17dead08f1679701023fc2a348b07abd7af

recipes-kernel/modules/kernel-module-vdmafb.bb
@@ -1,7 +1,7 @@
 SUMMARY = "Kernel driver module VDMA video driver"
 require kernel-module-topic.inc
 
-SRCREV = "${AUTOREV}"
+SRCREV = "325d34a89534dde63af3a78f69a12d7807a35b3c"
 
 RRECOMMENDS_${PN} = "\
 	kernel-module-backlight \