d58cb56521c8416a71f3da7533fc466f58c21d69
Mike Looijmans <mike.looijmans@topic.nl>
2016-11-03 12:43:33 +0100

    create-boot-archive.sh: Add script to create boot archive
    Filling the boot partition requires renaming some of the files. This
    script creates a tar file of the boot partition contents which can
    be extracted to a FAT partition. Set MACHINE and DTB environments
    before calling the script. The resulting tar is written to:
    tmp-glibc/deploy/images/${MACHINE}/boot.tar.gz

---
+++ scripts/create-boot-archive.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Script to create a tar file with the boot partition contents. Set DTB and
+# MACHINE environments first, e.g.:
+# MACHINE=topic-miami-florida-gen-xc7z015 DTB=uImage-topic-miami-florida-gen.dtb create-boot-archive.sh
+if [ -z "${SD_BOOTSCRIPT}" ]
+then
+	SD_BOOTSCRIPT=autorun.scr
+fi
+
+set -e
+IMAGE_ROOT=tmp-glibc/deploy/images/${MACHINE}
+if [ -d ${IMAGE_ROOT}/boot ]
+then
+	rm -rf ${IMAGE_ROOT}/boot
+fi
+mkdir ${IMAGE_ROOT}/boot
+cp ${IMAGE_ROOT}/BOOT.bin ${IMAGE_ROOT}/boot/BOOT.BIN
+cp ${IMAGE_ROOT}/u-boot.img ${IMAGE_ROOT}/boot/
+cp ${IMAGE_ROOT}/${SD_BOOTSCRIPT} ${IMAGE_ROOT}/boot/autorun.scr
+cp ${IMAGE_ROOT}/uImage ${IMAGE_ROOT}/boot/
+cp ${IMAGE_ROOT}/${DTB} ${IMAGE_ROOT}/boot/devicetree.dtb
+tar czf ${IMAGE_ROOT}/boot.tar.gz -C ${IMAGE_ROOT}/boot/ .
+rm -r ${IMAGE_ROOT}/boot
