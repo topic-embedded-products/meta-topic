topic-embedded-products/meta-topic

commit 8153b67bccbe2c525a985c78d7a1b2888e6c514f

devicetree: Fix florida test board pinmuxing
MIO 15 for GPIO expander IRQ
MIO 46..49 as GPIO for loopback testing

recipes-kernel/linux-zynq/linux-topic.bb
@@ -4,7 +4,7 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
 
 KBRANCH = "topic-miami"
-SRCREV = "c890395b68dd38415c6206846043d86a63508ae9"
+SRCREV = "d2d366f018b7bee88a6a82878bf72aff437167c8"
 LINUX_VERSION = "4.6"
 
 inherit kernel

commit 5b9c1eb182a150940e45e39788e9dcad447c1ca4

devicetree: Fix 7045 no longer booting on test board
Pin assignment for "miami" broke the "miamiplus", keep them apart
properly.

Fixes: 8153b67bccbe2c525a985c78d7a1b2888e6c514f

recipes-kernel/linux-zynq/linux-topic.bb
@@ -4,7 +4,7 @@ LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
 
 KBRANCH = "topic-miami"
-SRCREV = "d2d366f018b7bee88a6a82878bf72aff437167c8"
+SRCREV = "466dc04699a6e8766d0f3dfa62bbe7ccf11f24fe"
 LINUX_VERSION = "4.6"
 
 inherit kernel