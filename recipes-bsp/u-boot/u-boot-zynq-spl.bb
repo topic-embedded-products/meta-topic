topic-embedded-products/meta-topic

commit e0120eb2e952beb7c30304508e084a1ca30d2633

u-boot: Add license and rename "u-boot-zynq-git" to "u-boot-zynq-spl"
Make the patch directory match the recipe name, so that the filespath override
is no longer needed.

Add a LICENSE field, u-boot.inc from OE-core master no longer includes this line
by default. Not needed for morty, but it does no harm.

recipes-bsp/u-boot/u-boot-zynq-spl.bb
@@ -1,5 +1,5 @@
-# U-boot SPL boots without FSBL.
-FILESEXTRAPATHS_prepend := "${THISDIR}/u-boot-zynq-git:"
+LICENSE = "GPLv2+"
+LIC_FILES_CHKSUM = "file://Licenses/README;md5=a2c678cfd4a4d97135585cad908541c6"
 
 require recipes-bsp/u-boot/u-boot.inc
 

commit 6c6c6f9c49ddc6340131f741bd36477996d6d98b

u-boot-zynq-spl: Prioritize license information
The core u-boot.inc may set license information that does not match the
2017.01 version of u-boot that is required. Move the license below the
include so it takes precedence.

Fixes: e0120eb2e952beb7c30304508e084a1ca30d2633

recipes-bsp/u-boot/u-boot-zynq-spl.bb
@@ -1,7 +1,7 @@
+require recipes-bsp/u-boot/u-boot.inc
+
 LICENSE = "GPLv2+"
 LIC_FILES_CHKSUM = "file://Licenses/README;md5=a2c678cfd4a4d97135585cad908541c6"
-
-require recipes-bsp/u-boot/u-boot.inc
 
 DEPENDS += "dtc-native"
 