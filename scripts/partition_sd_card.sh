671c9c605693e533f3f0ca54900df7b495209e20
Mike Looijmans <mike.looijmans@topic.nl>
2019-10-28 10:58:58 +0100

    partition_sd_card.sh: Avoid "sfdisk -u" and don't attempt to mount
    The -uM commandline option is no longer supported by recent sfdisk, so avoid it.
    Increase the rootfs size and don't use bigalloc on the data partition.
    Don't attempt to mount the partitions, only leads to problems. Just unplug the
    card after running the script and put it back to have userspace mount it.

--- scripts/partition_sd_card.sh
+++ scripts/partition_sd_card.sh
@@ -9,9 +9,9 @@ fi
 set -e
 
 # Partition the disk, as 64M FAT16, 400MB root and the rest data
-sfdisk -uM $1 << EOF
-0 62 6
-,400
+sfdisk $1 << EOF
+1M 63M 6
+,960M
 ,
 
 EOF
@@ -20,14 +20,8 @@ EOF
 mkfs.vfat -n "boot" ${DEV}1
 
 # Format the Linux rootfs part
-mkfs.ext4 -m 0 -L "rootfs" -b 2048 -N 24000 -O sparse_super,dir_index  ${DEV}2
+mkfs.ext4 -m 0 -L "rootfs" -O sparse_super,dir_index  ${DEV}2
 
 # Format the Linux data part, optimize for large files
-mkfs.ext4 -m 0 -L "data" -O large_file,sparse_super,dir_index,bigalloc -C 262144 ${DEV}3
+mkfs.ext4 -m 0 -L "data" -O large_file,sparse_super,dir_index ${DEV}3
 
-mkdir /media/boot
-mount ${DEV}1 /media/boot
-mkdir /media/rootfs
-mount ${DEV}2 /media/rootfs
-mkdir /media/data
-mount ${DEV}3 /media/data
