topic-embedded-products/meta-topic

commit e6f292dc537a5f8769d78945008689ef21ea8175

fpga-firmware-load: Compile to $B and install in /usr/sbin
Make the do_compile() step output to ${B} so it behaves like a
proper compilation (not changing files in-place).
Since the script is intended for root/system place it in /usr/sbin.

recipes-bsp/fpga/fpga-firmware-load.bb
@@ -8,8 +8,6 @@ PACKAGES = "${PN}"
 
 SRC_URI = "file://init file://${BPN}.service file://${BPN}.sh"
 
-S = "${WORKDIR}"
-
 inherit update-rc.d systemd
 
 # Set to start at 03, which is before modutils
@@ -20,17 +18,19 @@ INITSCRIPT_PARAMS = "start 03 S ."
 SYSTEMD_SERVICE:${PN} = "${BPN}.service"
 
 do_compile() {
-	sed -i -e 's,@LIBDIR@,${nonarch_base_libdir},g' ${WORKDIR}/${BPN}.sh
-	sed -i -e 's,@BINDIR@,${bindir},g' ${WORKDIR}/${BPN}.service
+	for f in ${BPN}.sh ${BPN}.service init
+	do
+		sed -e 's,@LIBDIR@,${nonarch_base_libdir},g' -e 's,@BINDIR@,${sbindir},g' ${WORKDIR}/$f > ${B}/$f
+	done
 }
 
-FILES:${PN} = "${bindir} ${sysconfdir} ${systemd_unitdir}"
+FILES:${PN} = "${sbindir} ${sysconfdir} ${systemd_unitdir}"
 
 do_install() {
-	install -d ${D}${bindir}
-	install -m 755 ${WORKDIR}/${BPN}.sh ${D}${bindir}/${PN}.sh
+	install -d ${D}${sbindir}
+	install -m 755 ${B}/${BPN}.sh ${D}${sbindir}/${PN}.sh
 	install -d ${D}${sysconfdir}/init.d
 	install -m 755 ${WORKDIR}/init ${D}${sysconfdir}/init.d/${BPN}.sh
 	install -d ${D}${systemd_unitdir}/system
-	install -m 0644 ${WORKDIR}/${BPN}.service ${D}${systemd_unitdir}/system/
+	install -m 0644 ${B}/${BPN}.service ${D}${systemd_unitdir}/system/
 }

recipes-bsp/fpga/fpga-firmware-load/init
@@ -1,6 +1,6 @@
 #!/bin/sh
 case "$1" in
   start)
-    /usr/bin/fpga-firmware-load.sh
+    @BINDIR@/fpga-firmware-load.sh
     ;;
 esac

commit f43e255d656fd479603547b5c94395e298ada8ed

fpga-firmware-load: Fix @BINDIR@ remaining in init script
The recipe replaced @BINDIR@ in the init script but copied the original
from ${WORKDIR} instead of the output in ${B}

Fixes: e6f292d "fpga-firmware-load: Compile to $B and install in /usr/sbin"

recipes-bsp/fpga/fpga-firmware-load.bb
@@ -30,7 +30,7 @@ do_install() {
 	install -d ${D}${sbindir}
 	install -m 755 ${B}/${BPN}.sh ${D}${sbindir}/${PN}.sh
 	install -d ${D}${sysconfdir}/init.d
-	install -m 755 ${WORKDIR}/init ${D}${sysconfdir}/init.d/${BPN}.sh
+	install -m 755 ${B}/init ${D}${sysconfdir}/init.d/${BPN}.sh
 	install -d ${D}${systemd_unitdir}/system
 	install -m 0644 ${B}/${BPN}.service ${D}${systemd_unitdir}/system/
 }